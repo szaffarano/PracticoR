a6d4c38f49de19366489e17ce9d48a174640c41d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2016-04-23 15:07:32 -0300

    Intro básica

---
+++ ejercicios.R
@@ -0,0 +1,20 @@
+#
+# Prof: Marcelo Soria
+# soria@agro.uba.ar
+#
+# 24/4/2016
+#
+
+# Asignaciones: usar "=" y "<-" es equivalente.  Por claridad conviene "<-" (por ejemplo
+# para cuando se pasa a una función parámetros por nombre, ahi va "=")
+
+a <- 5
+ls()
+typeof(a)
+
+#
+# cheat sheets:
+# http://www.rstudio.com/wp-content/uploads/2016/03/rmarkdown-cheatsheet-2.0.pdf
+# http://www.rstudio.com/wp-content/uploads/2016/01/rstudio-IDE-cheatsheet.pdf
+# http://www.rstudio.com/wp-content/uploads/2015/12/ggplot2-cheatsheet-2.0.pdf
+# https://cran.r-project.org/doc/contrib/Short-refcard.pdf
